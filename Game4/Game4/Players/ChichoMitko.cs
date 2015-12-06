a5ccd5713131ad696071b4b637d8e12338a31fdd
rankonenig <mddragov@gmail.com>
2015-12-06 17:27:48 +0200

    Added ChichoMitko

---
+++ Game4/Game4/Players/ChichoMitko.cs
@@ -0,0 +1,59 @@
+﻿using Game4.Players;
+using Microsoft.Xna.Framework.Graphics;
+
+/// <summary>
+/// Marto made this class
+/// #ChichoMitko
+/// #Focus
+/// </summary>
+
+namespace Game4.PlayersAndClasses
+{
+    public class ChichoMitko : Player
+    {
+        #region fields
+        /// <summary>
+        /// Fields
+        /// </summary>
+        private int focus;
+
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        ///  Constructors
+        /// </summary>
+
+        public ChichoMitko(int x, int y) : base(x, y)
+        {
+
+        }
+
+        public ChichoMitko(int x, int y, Texture2D pic, double life, Ability miracleShot, int focus)
+           : base(x, y, pic, life, miracleShot)
+        {
+            this.Focus = 100;
+        }
+
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Properties
+        /// </summary>
+        public int Focus
+        {
+            get { return this.focus; }
+            set
+            {
+                if (value > 100)
+                {
+                    value += 100;
+                }
+                this.focus = value;
+            }
+        }
+
+        #endregion
+    }
+}
