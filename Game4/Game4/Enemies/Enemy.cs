eb6299191eb1498a3384d65c5765b4d1bc707af7
JaniFandi <janifarzandafandi@abv.bg>
2015-12-21 23:30:23 +0200

    Added class Enemy

---
+++ Game4/Game4/Enemies/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPGGame.Enemies
+{
+   abstract class Enemy
+    {
+       int health, damage;
+       public abstract int TakeDamage(Player player);
+       public abstract int HitDamage(Player player);
+
+        public Enemy(int health, int damage)
+        {
+            this.Health = health;
+            this.Damage = damage;
+        }
+
+        public int Damage
+        {
+            get { return this.damage; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Damage cannot be negative");
+                }
+                this.damage = value;
+            }
+        }
+
+        public int Health
+        {
+            get { return this.health; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Health cannot be negative");
+                }
+                this.health = value;
+
+            }
+        }
+
+
+    }
+}
