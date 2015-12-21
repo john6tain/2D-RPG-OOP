1b328fc292c2a252e3a0ceac0d017684335ec24a
JaniFandi <janifarzandafandi@abv.bg>
2015-12-21 23:30:57 +0200

    Added class Mob

---
+++ Game4/Game4/Enemies/Mob.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static RPGGame.Player;
+
+namespace RPGGame.Enemies
+{
+    class Mob : Enemy
+    {
+        public Mob(int health = 5000, int damage = 100) : base(health, damage)
+        { 
+        }
+
+
+        public override int TakeDamage(Player player)
+        {
+            this.Health = this.Health - player.Damage;
+            return this.Health;
+        }
+
+        public override int HitDamage(Player player)
+        {
+            player.Life = player.Life - this.Damage;
+            return player.Life;
+        }
+
+        
+    }
+}
