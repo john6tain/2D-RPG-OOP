225f9f84ee300b491e34a1942beb784d984b10c7
Alex <programmaker@abv.bg>
2015-12-22 02:20:10 +0200

    Fixed Git changes

--- Game4/Game4/Enemies/Mob.cs
+++ Game4/Game4/Enemies/Mob.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Graphics;
 using RPGGame.Players;
 
 namespace RPGGame.Enemies
@@ -33,35 +32,6 @@ namespace RPGGame.Enemies
         }
 
         #endregion
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using static RPGGame.Player;
 
-namespace RPGGame.Enemies
-{
-    class Mob : Enemy
-    {
-        public Mob(int health = 5000, int damage = 100) : base(health, damage)
-        { 
-        }
-
-
-        public override int TakeDamage(Player player)
-        {
-            this.Health = this.Health - player.Damage;
-            return this.Health;
-        }
-
-        public override int HitDamage(Player player)
-        {
-            player.Life = player.Life - this.Damage;
-            return player.Life;
-        }
-
-        
->>>>>>> origin/master
     }
 }
